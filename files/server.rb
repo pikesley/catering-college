82a5e6c4d8f63b456ef35ebdf67688de5c97d922
Chef <chef@chef-server.(none)>
2012-02-18 00:25:42 +0000

    Will now install everything required

---
+++ files/server.rb
@@ -0,0 +1,36 @@
+log_level          :info
+log_location       STDOUT
+ssl_verify_mode    :verify_none
+chef_server_url    "http://SERVER_NAME:4000"
+
+signing_ca_path    "/var/chef/ca"
+couchdb_database   'chef'
+
+cookbook_path      [ "/var/chef/cookbooks", "/var/chef/site-cookbooks" ]
+
+file_cache_path    "/var/chef/cache"
+node_path          "/var/chef/nodes"
+openid_store_path  "/var/chef/openid/store"
+openid_cstore_path "/var/chef/openid/cstore"
+search_index_path  "/var/chef/search_index"
+role_path          "/var/chef/roles"
+
+validation_client_name "chef-validator"
+validation_key         "/etc/chef/validation.pem"
+client_key             "/etc/chef/client.pem"
+web_ui_client_name     "chef-webui"
+web_ui_key             "/etc/chef/webui.pem"
+
+web_ui_admin_user_name "admin"
+web_ui_admin_default_password "PASSWORD"
+
+supportdir = "/var/chef/support"
+solr_jetty_path File.join(supportdir, "solr", "jetty")
+solr_data_path  File.join(supportdir, "solr", "data")
+solr_home_path  File.join(supportdir, "solr", "home")
+solr_heap_size  "256M"
+
+umask 0022
+
+Mixlib::Log::Formatter.show_time = false
+
